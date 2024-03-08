94263bc725ca92781fc8c87614c8abd134b7e6fe
EmilHack <mymagic236@gmail.com>
2024-03-09 00:40:49 +0300

    initial commit

--- main3.py
+++ main3.py
@@ -1,27 +1,18 @@
 import pygame
 pygame.init()
-
-
 clock = pygame.time.Clock()
-
 screen = pygame.display.set_mode((900,600), pygame.RESIZABLE) #, flags=pygame.NOFRAME
-# flags=pygame.NOFRAME убирает верхнии кнопки
-
 pygame.display.set_caption("КАЗИНО ОНЛАЙН")
 icon = pygame.image.load('images/icon.png').convert_alpha()
 pygame.display.set_icon(icon)
-
-
-
 label = pygame.font.Font('font/RubikGlitchPop-Regular.ttf', 50)
-lose_label = label.render('ПОРАЖЕНИЕ', False, (255, 255, 255))
+lose_label = label.render('ОПУЩЕН', False, (255, 255, 255))
 restart_label = label.render('ИГРАТЬ СНОВА', False, (14, 200, 255))
 restart_label_rect = restart_label.get_rect(topleft=(270, 300))
 bg = pygame.image.load('images/bg.jpg').convert_alpha()
-
-
-bg_sound = pygame.mixer.Sound('sound/mia.mp3')
-
+bgover = pygame.image.load('images/image.psd(10).png').convert_alpha()
+bg_sound = pygame.mixer.Sound('sound/gedagedago (128kbps).mp3')
+bg_soundEND = pygame.mixer.Sound('sound/NVP.mp3')
 ''' функции '''
 def draw_background():
     screen.blit(bg, (bg_x, -100))
@@ -72,7 +63,6 @@ def animends():
         screen.blit(walk_left[player_anim_count], (player_x, player_y))
     else:
         screen.blit(walk_right[player_anim_count], (player_x, player_y))
-
     if player_anim_count == 5:  # переключение анимации
         player_anim_count = 0
     else:
@@ -88,7 +78,6 @@ def restartik():
         ghost_in_game.clear()
         bg_x = 0
         bg_x2 = bg_x + 900
-''' запуск и -запуск'''
 def iventik():
     global running
     pygame.display.update()  # обновление экрана
@@ -98,16 +87,17 @@ def iventik():
             pygame.quit()
         if event.type == ghost_timer:
             ghost_in_game.append(ghost.get_rect(topleft=(900, 500)))
-
-
-ghost = pygame.image.load('images/ghost (2).png').convert_alpha()
+def over():
+    global bgO_x1, bgO_x2
+    bgO_x1 = 0
+    bgO_x2 = 900
+    screen.blit(bgover, (bgO_x1, 0))
+    screen.blit(bgover, (bgO_x2, -100))
+ghost = pygame.image.load('images/ukr.png').convert_alpha()
 ghost_in_game = []
 ghost_timer = pygame.USEREVENT + 2
 pygame.time.set_timer(ghost_timer, 1000)
-
-
 bg_sound.play()
-#спрайты
 def walk():
     global walk_left, walk_right
     walk_left = [
@@ -124,26 +114,17 @@ def walk():
     pygame.image.load('images/sprite_right/s4.png').convert_alpha(),
     pygame.image.load('images/sprite_right/s5.png').convert_alpha(),
     pygame.image.load('images/sprite_right/s6.png').convert_alpha()]
-
 player_anim_count = 0
 bg_x = 0
-
-#5
 player_speed = 5
 player_x = 60
 player_y = 500
 is_jump = False
 jump_count = 7
-
 bg_x2 = 900
-
-
 gameplay = True
-
 running = True
 while True: # цикл, чтобы экран был включен
-
-
     if gameplay:
         walk()
         draw_background()
@@ -154,15 +135,16 @@ while True: # цикл, чтобы экран был включен
         kittyjimp()
         animends()
     else:
-        screen.fill((0,0,0))
+        over()
+
         screen.blit(lose_label,  (300, 200))
         screen.blit(restart_label, restart_label_rect)
         bg_sound.stop()
+        bg_soundEND.play()
         mouse = pygame.mouse.get_pos()
         restartik()
     iventik()
 #скорость игры
     clock.tick(30)
-
 
 
