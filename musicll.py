5a43d7bf49c52f2c5839679682417243f46bccb2
EmilHack <mymagic236@gmail.com>
2024-03-11 23:35:32 +0300

    опять музыка

--- musicll.py
+++ musicll.py
@@ -7,10 +7,12 @@ tracks = [
     'sound/Icyk_Cyper_feat_Igor_cyba_-_Dymok_76563985.mp3',
     'sound/gedagedago (128kbps).mp3']
 
+
 def random_track():
     global random_number
-    # Генерация случайного числа от 0 до 3
     random_number = random.randint(0, 3)
+
+
 def treck1():
     global music_player, tracks
     random_track()
@@ -22,12 +24,31 @@ def treck1():
          music_player = False
 
 
+def random_track_over():
+    global random_number2
+    random_number2 = random.randint(0, 6)
+
+
+bg_soundEND = [
+    'sound/over/NVP.mp3',
+    'sound/over/nvp2.mp3',
+    'sound/over/Akira_Yamaoka_Silent_Hill_3_OST_14_Never_Forgive_Me_Never_Forget.mp3',
+    'sound/over/C418_-_Haggstrom_30921643.mp3',
+    'sound/over/C418_-_Minecraft_30921694.mp3',
+    'sound/over/C418_-_Subwoofer_Lullaby_30921632.mp3',
+    'sound/over/KXBRIGU - AFRAID_ Buckshot Roulette Phonk.mp3'
+]
+
+
+
+
+
 def treck2():
-    global music_player
+    global music_player, bg_soundEND
+    random_track_over()
     if music_player:
         pygame.mixer.music.stop()
-        bg_soundEND = 'sound/NVP.mp3'
-        pygame.mixer.music.load(bg_soundEND)
+        pygame.mixer.music.load(bg_soundEND[random_number2])
         pygame.mixer.music.play()
         music_player = False
     if not pygame.mixer.music.get_busy():
